3436b4ec39ccd51510fd5a9b51a68434910c5344
killarock <yury.beloborodov@gmail.com>
2019-12-30 13:24:59 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require(`path`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+const MiniCssExtractPlugin = require(`mini-css-extract-plugin`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `build`)
+  },
+  devtool: `source-map`,
+  devServer: {
+    port: 1337,
+    hot: true,
+    compress: true,
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: `babel-loader`,
+        options: {
+          presets: [`@babel/preset-env`],
+          plugins: [`@babel/plugin-transform-runtime`]
+        }
+      },
+      {
+        test: /\.s[ca]ss$/,
+        use: [MiniCssExtractPlugin.loader, `css-loader`, `sass-loader`]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: `api-practise`,
+      template: `./public/index.html`
+    }),
+    new MiniCssExtractPlugin({
+      filename: `[name].css`
+    })
+  ]
+};
